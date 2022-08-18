19937d60384c456116f10e7356b7d2dd03f59a19
Cristian <cristian.coppari@gmail.com>
2022-08-18 19:43:05 -0300

    finished the home

--- src/data/index.js
+++ src/data/index.js
@@ -6,6 +6,49 @@ const indexData = {
   metaDescription: "Somos la mejor empresa de seguridad privada",
 
   header,
+
+  hero: {
+    title: "LA MEJOR SOLUCIÓN <br> EN <strong>VIGILANCIA.</strong>",
+
+    image: {
+      mobile: "/images/hero-home.png",
+      tablet: "/images/hero-home.png",
+      desktop: "/images/hero-home.png",
+      desktopXL: "/images/hero-home.png",
+    },
+
+    cta: {
+      link: "/servicios",
+
+      label: "IR A SERVICIOS"
+    }
+  },
+
+  calidadSeguridadCompromiso: {
+    title: "Calidad <br>Seguridad <br>Compromiso",
+
+    text: [
+      "Alertas Seguridad Privada. Experiencia, responsabilidad y confiabilidad, puesta al servicio de su seguridad."
+    ],
+
+    image: "/images/services.png"
+  },
+
+  estamosParaAyudarte: {
+    title: "Estamos <br>para ayudarte",
+
+    text: [
+      "Comunicate con nosotros llamando a nuestro teléfono, enviando un mensaje por WhatsApp o sino, envianos tu consulta completando el formulario, para que uno de nuestros asesores se ponga en contacto"
+    ],
+
+    image: "/images/security-guard.png",
+
+    cta: {
+      link: "/contacto",
+
+      label: "CONTACTANOS"
+    }
+  }
 }
 
 module.exports = indexData;
