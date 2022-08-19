1ab56c26cb1544080ef7823c3572eb43d726e67d
Cristian <cristian.coppari@gmail.com>
2022-08-19 01:05:08 -0300

    working in contactanos

---
+++ src/data/contacto.js
@@ -0,0 +1,21 @@
+const header = require("./header.js")
+
+const indexData = {
+  title: "Contacto - Alertas Seguridad Privada",
+
+  metaDescription: "Somos la mejor empresa de seguridad privada",
+
+  header,
+
+  contacto: {
+    title: "Contacto",
+
+    body: [
+      "<strong>¡Estamos para ayudarte!</strong>",
+      "Comunícate con nosotros llamando a nuestro teléfono, enviando un mensaje por whatsapp o sino, envianos tu consulta completando el formulario, para que uno de nuestros asesores se ponga en contacto.",
+      "Para cotizaciones, por favor seleccioná el Servcio que desees contratar."
+    ]
+  }
+}
+
+module.exports = indexData;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-const indexData = require("../data/index.js")
+const indexData = require("../data/index.js");
+const contactoData = require("../data/contacto.js");
 
 // Home page
 router.get('/', function (req, res, next) {
   res.render('index', indexData);
 });
+
+router.get("/contacto", function (req, res) {
+  res.render("contacto", contactoData);
+})
 
 module.exports = router;
