0286f69a07a46371ef1e19721d0d13a87ddb7e4a
Cristian <cristian.coppari@gmail.com>
2023-03-19 17:38:33 -0300

    installed compression and helmet with --save because i have an error in heroku

--- app.js
+++ app.js
@@ -13,26 +13,26 @@ var app = express();
 app.set('views', path.join(__dirname, "src", 'views'));
 app.set('view engine', 'twig');
 
-// app.use(compression());
-// app.use(helmet({
-//   contentSecurityPolicy: {
-//     directives: {
-//       defaultSrc: ["'self'"],
-//       scriptSrc: [
-//         "'self'",
-//         "'unsafe-inline'", // To allow inline scripts
-//         "https://cdn.jsdelivr.net", // To allow scripts from the external CDN
-//       ],
-//       // styleSrc: ["'self'", "https:", "'unsafe-inline'"],
-//       // imgSrc: ["'self'", "data:", "https:"],
-//       // connectSrc: ["'self'", "https:"],
-//       // fontSrc: ["'self'", "https:", "data:"],
-//       // objectSrc: ["'none'"],
-//       // mediaSrc: ["'self'", "https:", "data:"],
-//       // frameSrc: ["'self'", "https:"],
-//     },
-//   },
-// }));
+app.use(compression());
+app.use(helmet({
+  contentSecurityPolicy: {
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: [
+        "'self'",
+        "'unsafe-inline'", // To allow inline scripts
+        "https://cdn.jsdelivr.net", // To allow scripts from the external CDN
+      ],
+      // styleSrc: ["'self'", "https:", "'unsafe-inline'"],
+      // imgSrc: ["'self'", "data:", "https:"],
+      // connectSrc: ["'self'", "https:"],
+      // fontSrc: ["'self'", "https:", "data:"],
+      // objectSrc: ["'none'"],
+      // mediaSrc: ["'self'", "https:", "data:"],
+      // frameSrc: ["'self'", "https:"],
+    },
+  },
+}));
 
 app.use(logger('dev'));
 app.use(express.json());
